d8338a58e69df9652cfa0d3e23476c0e534157ca
Kyle Sherrington <kylejsherrington@gmail.com>
2020-12-18 20:47:54 -0800

    Adding more Event Listeners

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var intro = document.getElementById("intro");
 var headshot = document.getElementById("headshot");
-
+var productionPara = document.getElementById("productionPara");
 
 intro.addEventListener("click", function(){ intro.style.background = "lightblue"});
 headshot.addEventListener("click", function(){ headshot.style.background = "lightblue"});
+productionPara.addEventListener("click", function(){ productionPara.style.background = "lightblue"});
