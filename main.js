4f9a1b65b6da83bc4468787f9fb92f0f56143457
Kyle Sherrington <kylejsherrington@gmail.com>
2020-12-18 20:46:59 -0800

    Adding some more JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 var intro = document.getElementById("intro");
+var headshot = document.getElementById("headshot");
+
 
 intro.addEventListener("click", function(){ intro.style.background = "lightblue"});
+headshot.addEventListener("click", function(){ headshot.style.background = "lightblue"});
